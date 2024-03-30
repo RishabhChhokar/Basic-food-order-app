1be00368f1950874eb4668a81dd5d3494cd179f5
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-03-30 17:48:48 +0530

    Added useState hook for simpler data transfer.

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,14 +1,19 @@
 import CartLogo from "../Cart/CartLogo";
 import classes from "./CartButton.module.css";
-
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 const CartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartLogo />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
